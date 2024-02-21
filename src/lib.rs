19b9033db9d0302331b9cf2abecaecdbbcddf9a8
abdur-raqeeb ajao <ajaorqb@gmail.com>
2024-02-21 13:05:13 -0700

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+mod utils;
+
+use image::RgbaImage;
+use image::imageops;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn shrink_by_half(original_image: Vec<u8>, width: u32, height: u32) -> Vec<u8> {
+    let image: RgbaImage = image::ImageBuffer::from_vec(width, height, original_image).unwrap();
+    let output_image = imageops::resize(&image, width / 2, height / 2, imageops::FilterType::Nearest);
+
+    output_image.into_vec()
+}
